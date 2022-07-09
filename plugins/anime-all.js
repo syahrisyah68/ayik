b14f4c45d2b8b165fdb65fe272029f6c805bb7e7
nsrstory <76632542+syahrisyah68@users.noreply.github.com>
2022-07-09 09:43:48 +0700

    Update anime-all.js

--- plugins/anime-all.js
+++ plugins/anime-all.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, command }) => {
 let nyenye = `https://api-reysekha.herokuapp.com/api/wallpaper/${command}?apikey=APIKEY`
-    conn.sendButtonImg(m.chat, nyenye, 'Nih', wm2, 'Next', `.${command}`, m) 
+    conn.sendButtonImg(m.chat, nyenye, 'SYAHRI-BOT', wm2, 'Next', `.${command}`, m) 
 }
 handler.help = ['akira', 'akiyama', 'anna', 'asuna', 'ayuzawa', 'boruto', 'chiho', 'chitoge', 'deidara', 'erza', 'elaina', 'eba', 'emilia', 'hestia', 'hinata', 'inori', 'isuzu', 'itachi', 'itori', 'kaga', 'kagura', 'kaori', 'keneki', 'kotori', 'kurumi', 'madara', 'mikasa', 'miku', 'minato', 'naruto', 'nezuko', 'sagiri', 'sasuke', 'sakura', 'cosplay']
 handler.tags = ['anime']
